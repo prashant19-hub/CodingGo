10d1d1ba2e71b9c28ce3b5b493889b94de2277a9
prashant19-hub <prasha5711nt@gmail.com>
2026-03-13 20:52:51 +0530

    Importable Executable

---
+++ importExecutable/calculator/calc.go
@@ -0,0 +1,33 @@
+package calculator
+import "fmt"
+
+type User struct{
+	Name string
+	Contno int
+	id string
+}
+
+type student struct{
+	Name string
+	Rollno int
+	class int
+	marks int
+}
+
+func Add(a ,b int) int{
+	fmt.Println("add func call hua")
+	return a+b
+}
+ func Sub(a ,b int) int{
+	 return a-b
+ }
+func Multiply(a ,b int) int{
+	return a*b
+}
+func Divide(a ,b int) int{
+	return a/b
+}
+func add(a ,b int) int{    //  this func only work and used only this package because there in func small letter (sub) 
+                           // if the func is start letter is capital then they use every package in
+	return a-b
+}
